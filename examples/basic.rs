cd7a465912bd1edf2d2dc4e0deecb6b91c8c17fd
Sigma-dev <antonin.programming@gmail.com>
2024-09-24 22:53:46 +0200

    Made the example better

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,32 +3,38 @@ use client::SteamP2PClient;
 use prelude::*;
 use bevy_steam_p2p::*;
 
+
+
 fn main() {
     App::new()
     .add_plugins(SteamP2PPlugin)
     .add_plugins(DefaultPlugins)
     .add_systems(Startup, startup)
-    .add_systems(Update, lobby_joined)
+    .add_systems(Update, update)
     .run();
 }
 
 fn startup(
     mut commands: Commands,
-    mut client: ResMut<SteamP2PClient>
 ) {
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(5., 5., 5.).looking_at(Vec3::new(0.0, 0., 0.0), Vec3::Y),
         ..default()
     });
-
-    client.create_lobby(8);
 }
 
-fn lobby_joined(
-    mut event_reader: EventReader<LobbyJoined>,
+/*
+    1. Have one of your clients create a server (PRESS C)
+    2. Have the other clients join using the steam friends list
+    3. Have fun with replicated objects
+*/
+fn update(
     mut client: ResMut<SteamP2PClient>,
     keys: Res<ButtonInput<KeyCode>>,
 ) {
+    if keys.just_pressed(KeyCode::KeyC) {
+        client.create_lobby(8);
+    }
     if keys.just_pressed(KeyCode::KeyT) {
         client.send_message_all(NetworkData::DebugMessage("Hello world !".to_owned()), SendFlags::RELIABLE);
     }
